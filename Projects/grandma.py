93efc15263bd6fd3c16b5792bea47f777ed8f049
octave89 <elaraemery@seattleacademy.org>
2024-09-13 20:57:26 +0000

    grandma

---
+++ Projects/grandma.py
@@ -0,0 +1,10 @@
+while True:
+
+    word = input("What do you think Grandma likes?")
+
+    if "g" in word or "r" in word or "a" in word or "n" in word or "d" in word or "m" in word: 
+        print(f"Grandma doesn't like {word}!")
+    else:
+        print(f"Grandma likes word!")
+
+    print("")
